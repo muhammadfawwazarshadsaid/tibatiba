ac6d9adb733dae10bd16522ef39489088a96c3ac
rigifdaga <rfdauliaa@gmail.com>
2024-08-25 12:14:55 +0700

    modifying gemini ai prompt

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,42 +1,44 @@
 import base64
-from datetime import datetime
-
+import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.db import connection
-
 import google.generativeai as genai
 
 genai.configure(api_key="AIzaSyDw8pJxo89LRpKy6UVJ_79hQVbSvY1fvHY")
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-
-def get_adjusted_query(user_query):
-    response = model.generate_content("Given the user query '{user_query}', adjust it to match the data in the following format: place_description or post_caption.")
-    
-    adjusted_query = response.text
-    return adjusted_query
+def extract_key_terms(user_query):
+    response = model.generate_content(f"Identify key location or relevant terms from the following query in one word: '{user_query}'")
+    key_terms = response.text.strip().lower()
+    return key_terms
 
 @csrf_exempt
 def search_posts(request):
     if request.method == 'POST':
-        query = request.POST.get('query', '')
-        adjusted_query = get_adjusted_query(query)
+        body = json.loads(request.body)
+        query = body.get('query', '')
+        
+        key_terms = extract_key_terms(query)
+        
+        print(f"Extracted Key Terms: '{key_terms}'")
+        
+        if not key_terms:
+            return JsonResponse({'error': 'Could not extract meaningful terms from the query.'}, status=400)
         
         with connection.cursor() as cursor:
             cursor.execute("""
                 SELECT p.post_id, p.place_photo, p.place_city, p.place_description, p.post_caption, p.post_date, u.name AS user_name
                 FROM "Post" p
                 JOIN "User" u ON p.user_id = u.user_id
-                WHERE to_tsvector('indonesian', p.place_description || ' ' || p.post_caption) @@ to_tsquery('indonesian', %s)
-                """, [adjusted_query])
+                WHERE to_tsvector('indonesian', p.place_description || ' ' || p.post_caption || ' ' || p.place_city) @@ plainto_tsquery('indonesian', %s)
+                """, [key_terms])
             posts = cursor.fetchall()
         
         post_list = []
         for post in posts:
             place_photo = post[1]
-            # Convert binary data to base64-encoded string
             if place_photo:
                 place_photo = base64.b64encode(place_photo).decode('utf-8')
             post_list.append({
